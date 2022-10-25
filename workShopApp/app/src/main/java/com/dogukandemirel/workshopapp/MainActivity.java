fd3f34f2b86ce6bca4c47b90ceec6795104bebcf
dogukandmrl <demirel.dogukan@yahoo.com>
2022-10-25 13:38:33 +0300

    firebase auth is done
    login and register activities finished

--- workShopApp/app/src/main/java/com/dogukandemirel/workshopapp/MainActivity.java
+++ workShopApp/app/src/main/java/com/dogukandemirel/workshopapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.dogukandemirel.workshopapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView register;
+    private TextView register,forgotPassword;
+    private EditText editTextEmail,editTextPassword;
+    private Button signIn;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         register = (TextView) findViewById(R.id.twregister);
         register.setOnClickListener(this);
+
+        signIn = (Button) findViewById(R.id.signIn);
+        signIn.setOnClickListener(this);
+
+        editTextEmail = (EditText)  findViewById(R.id.email);
+        editTextPassword = (EditText)  findViewById(R.id.password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        forgotPassword = (TextView) findViewById(R.id.forgotPassword);
+        forgotPassword.setOnClickListener(this);
+
     }
 
     @Override
@@ -25,7 +55,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case  R.id.twregister:
                 startActivity(new Intent(this,RegisterUser.class));
                         break;
-
+            case R.id.signIn:
+                userLogin();
+                        break;
+            case R.id.forgotPassword:
+                startActivity(new Intent(this,ForgotPassword.class));
+                        break;
         }
+    }
+
+    private void userLogin() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if(email.isEmpty()){
+            editTextEmail.setError("Email alanı boş bırakılamaz");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Geçerli Email giriniz.");
+            editTextEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            editTextPassword.setError("Şifre Gereklidir");
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (password.length()<8){
+            editTextPassword.setError("Şifre minimum 8 karakterden oluşmalıdır");
+            editTextPassword.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                }else{
+                    Toast.makeText(MainActivity.this,"Girişte Hata oluştu.",Toast.LENGTH_LONG).show();
+
+                }
+            }
+        });
+
     }
 }
